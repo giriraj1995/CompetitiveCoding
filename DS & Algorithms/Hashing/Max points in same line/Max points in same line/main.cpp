8c6e52ee8edc203daab012ea56e4d57950fdb723
giriraj1995 <girirajsaigal@gmail.com>
2020-09-20 15:05:46 +0530

    max points in  a line

---
+++ DS & Algorithms/Hashing/Max points in same line/Max points in same line/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  Max points in same line
+//
+//  Created by Giriraj Saigal on 20/09/20.
+//  Copyright © 2020 Giriraj Saigal. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int max(int a, int b){
+    return a > b ? a:b;
+}
+
+int maxPoints(vector<int> x, vector<int> y) {
+
+    int n = (int)x.size();
+    if(n<=2) return n;
+    int max_points = 2;
+    
+    unordered_map<double,int> map;
+    
+    for(int i=0;i<n;i++){
+        int same_points = 0;
+        int inf_slope = 0; // verticle points
+        int curr_max = 0;
+        for(int j=i+1;j<n;j++){
+            if(x[i]==x[j] && y[i]==y[j]) same_points++; // avoinding 0/0 case
+            else if(x[i]==x[j]){        // avoiding division by 0
+                inf_slope++;
+                curr_max = max(curr_max,inf_slope);
+            }else{
+                double slope = (y[j]-y[i])/((x[j]-x[i])*1.0);
+                map[slope]++;
+                curr_max = max(curr_max,map[slope]);
+            }
+        }
+        max_points = max(max_points,curr_max + same_points + 1);
+        map.clear();
+    }
+    
+    return max_points;
+    
+}
+
+int main(int argc, const char * argv[]) {
+    cout<<maxPoints({0,1,-1}, {0,1,-1})<<endl;
+    return 0;
+}
---
+++ DS & Algorithms/Heap/Kth Smallest Element/Kth Smallest Element/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Kth Smallest Element
+//
+//  Created by Giriraj Saigal on 19/09/20.
+//  Copyright © 2020 Giriraj Saigal. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+
+
+int main(int argc, const char * argv[]) {
+    vector<int> q = {10,54,3,6,7,2,33};
+    int k = 3;
+    return 0;
+}
