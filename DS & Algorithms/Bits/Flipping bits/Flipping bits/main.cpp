72053f241bc9f19156609f790b074757fd353ef2
giriraj1995 <girirajsaigal@gmail.com>
2020-09-29 14:11:09 +0530

    flipper bits

---
+++ DS & Algorithms/Bits/Flipping bits/Flipping bits/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Flipping bits
+//
+//  Created by Giriraj Saigal on 29/09/20.
+//  Copyright © 2020 Giriraj Saigal. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+using namespace std;
+
+
+long flippingBits(long n) {
+    long flipper = pow(2,32)-1;
+    return flipper^n;
+
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    flippingBits(1);
+    return 0;
+}
