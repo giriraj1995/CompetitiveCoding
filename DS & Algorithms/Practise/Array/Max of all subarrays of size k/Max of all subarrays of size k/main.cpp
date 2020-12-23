72c291db9bb746ae7514f51acb49385df8b1aadc
giriraj1995 <girirajsaigal@gmail.com>
2020-12-23 15:08:07 +0530

    max of all subarrays of size k

--- DS & Algorithms/Practise/Array/Max of all subarrays of size k/Max of all subarrays of size k/main.cpp
+++ DS & Algorithms/Practise/Array/Max of all subarrays of size k/Max of all subarrays of size k/main.cpp
@@ -7,9 +7,51 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
 
+vector<int> maxOfSubArray(vector<int> x, int k) {
+    
+    queue<pair<int,int>> q;
+    vector<int> ans;
+    int i = 0;
+    while(i < k) {
+        
+        while(q.size() > 0 && q.front().first < x[i]){
+            q.pop();
+        }
+        
+        q.push({x[i], i});
+        i++;
+    }
+    
+    ans.push_back(q.front().first);
+    
+    while(i < x.size()) {
+        
+        while(q.size() > 0 && q.front().second <= i - k) {
+            q.pop();
+        }
+        
+        while(q.size() > 0 && q.front().first < x[i]){
+            q.pop();
+        }
+        
+        q.push({x[i],i});
+        i++;
+        ans.push_back(q.front().first);
+    }
+    
+    return ans;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<int> x = {1,3,-1,-3,5,3,6,7};
+    maxOfSubArray(x, 3);
     return 0;
 }
