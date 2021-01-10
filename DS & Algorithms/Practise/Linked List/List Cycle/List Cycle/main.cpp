47cf41c35fd474c68725f37ea96f1ccf301323f9
giriraj1995 <girirajsaigal@gmail.com>
2021-01-10 22:22:40 +0530

    list cycle

---
+++ DS & Algorithms/Practise/Linked List/List Cycle/List Cycle/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  List Cycle
+//
+//  Created by Giriraj Saigal on 10/01/21.
+//  Copyright © 2021 Giriraj Saigal. All rights reserved.
+//
+
+#include <iostream>
+
+
+//  Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+ListNode* detectCycle(ListNode* A) {
+    if(A == NULL)
+        return NULL;
+        
+    ListNode* slow = A;
+    ListNode* fast = A->next;
+
+    while(fast != slow && fast != NULL) {
+        slow = slow->next;
+        fast = fast->next;
+        
+        if(fast != NULL) {
+            fast = fast->next;
+        }else{
+            return NULL;
+        }
+    }
+
+    slow = A;
+    
+    while(slow->next) {
+        fast = slow->next;
+        slow->next = NULL;
+        slow = fast;
+    }
+    
+    return slow;
+
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
