76ee2bdfa002e1483fb9ac0c3c9405615a7c51c3
giriraj1995 <girirajsaigal@gmail.com>
2021-01-12 14:33:15 +0530

    longest subarray

--- DS & Algorithms/Practise/Maps/Longest Subarray Length/Longest Subarray Length/main.cpp
+++ DS & Algorithms/Practise/Maps/Longest Subarray Length/Longest Subarray Length/main.cpp
@@ -18,36 +18,33 @@ using namespace std;
 
 int solve(vector<int> A) {
     
-    int j = 1;
-    
+    int j = 0;
+    unordered_map<int, int> mp;
     for(int i = 0; i < A.size(); i++) {
         if(A[i] == 0)
             A[i] = -1;
     }
     
-    int maxSum = A[0];
-    int curSum = A[0];
+    int curSum = 0;
     int ans = 0;
-    int i = 0;
     
+    mp[0] = -1;
+    int maxLength = 0;
     while(j < A.size()) {
+        curSum += A[j];
         
-        if(curSum + A[j] >= A[j]) {
-            curSum = curSum + A[j];
-            
-            if(curSum == 1)
-                ans = max(j - i + 1, ans);
-        }else{
-            curSum = A[j];
-            i = j;
-            
-            if(curSum == 1)
-                ans = max(j - i + 1, ans);
+        if(mp.find(curSum - 1) != mp.end()) {
+            maxLength = max(maxLength, j - mp[curSum - 1]);
         }
+        
+        if(mp.find(curSum) == mp.end()) {
+            mp[curSum] = j;
+        }
+        
         j++;
     }
     
-    return ans;
+    return maxLength;
 }
 
 int main(int argc, const char * argv[]) {
