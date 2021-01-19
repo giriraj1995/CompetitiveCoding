865a6fc448289c21efaef44054003f7f8e83b99d
giriraj1995 <girirajsaigal@gmail.com>
2021-01-19 18:37:55 +0530

    Ath palindromic nu,ber

--- DS & Algorithms/Practise/Graphs/Ath palindromic no/Ath palindromic no/main.cpp
+++ DS & Algorithms/Practise/Graphs/Ath palindromic no/Ath palindromic no/main.cpp
@@ -7,9 +7,52 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+long getValue(string s){
+    long num  = 0;
+    for(long i=0;i<s.size();i++){
+        num = num*2 + (s[i]-'0');
+    }
+    return num;
+}
+long solve(int A) {
+    if(A==1) return A;
+    A--;
+    queue<string> q;
+    q.push("11");
+    while(!q.empty()){
+        string s = q.front();
+        q.pop();
+        A--;
+        if(!A){
+            return getValue(s);
+        }
+        int mid = s.size()/2;
+        if(s.size()%2==0){
+            string s0 = s, s1=s;
+            s0.insert(mid,"0");
+            s1.insert(mid,"1");
+            q.push(s0);
+            q.push(s1);
+        } else{
+            string ch(1,s[mid]);
+            string temp = s;
+            temp.insert(mid,ch);
+            q.push(temp);
+        }
+    }
+    return 0;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout<<solve(1000000)<<endl;
     return 0;
 }
