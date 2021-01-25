78e6ef6842736d9075a95e89d91dd3be7d43e153
giriraj1995 <girirajsaigal@gmail.com>
2021-01-25 22:26:45 +0530

    min max

---
+++ DS & Algorithms/Season 2/Arrays/Min Max/Min Max/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Min Max
+//
+//  Created by Giriraj Saigal on 25/01/21.
+//  Copyright © 2021 Giriraj Saigal. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+int solve(vector<int> &A) {
+    int low = INT_MAX;
+    int high = INT_MIN;
+    
+    for(int i : A) {
+        low = min(low, i);
+        high = max(high, i);
+    }
+    
+    return low + high;
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
