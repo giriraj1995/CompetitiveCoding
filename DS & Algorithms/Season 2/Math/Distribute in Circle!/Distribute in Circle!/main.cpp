4d86d6c000ceaadb447bdbaa511492fcd2d856d1
giriraj1995 <girirajsaigal@gmail.com>
2021-02-03 14:15:16 +0530

    Distribution in circle

---
+++ DS & Algorithms/Season 2/Math/Distribute in Circle!/Distribute in Circle!/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Distribute in Circle!
+//
+//  Created by Giriraj Saigal on 03/02/21.
+//  Copyright © 2021 Giriraj Saigal. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+int solve(int A, int B, int C) {
+    
+    return (C + (A -1)) % B;
+    
+}
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
