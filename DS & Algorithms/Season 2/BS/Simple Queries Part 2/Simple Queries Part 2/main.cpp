4690b7d14bad5c9b985e22f96f03b2b237e10f71
Giriraj <girirajsaigal@gmail.com>
2021-02-23 11:57:55 +0530

    Simple Queries

--- DS & Algorithms/Season 2/BS/Simple Queries Part 2/Simple Queries Part 2/main.cpp
+++ DS & Algorithms/Season 2/BS/Simple Queries Part 2/Simple Queries Part 2/main.cpp
@@ -7,16 +7,25 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+#define mod 1000000007
 
 int getdivisor(int x) {
     int ans = 1;
-    for(int i = 2; i * i <= x; i++) {
+    for(int i = 1; i * i <= x; i++) {
         if(x % i == 0) {
             if(x / i == i) {
-                ans *= i;
+                ans = (ans*i) % mod;
             }else{
-                ans *= i;
-                ans *= x/i;
+                ans = (ans*i) % mod;
+                ans = (ans*x/i) % mod;
             }
         }
     }
@@ -25,11 +34,11 @@ int getdivisor(int x) {
 }
 
 bool cmd(pair<int,long long> x, pair<int,long long> y) {
-    x.first > y.first;
+    return x.first > y.first;
 }
 
-vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
-    int n = A.size();
+vector<int> solve(vector<int> A, vector<int> B) {
+    int n = (int)A.size();
     vector<int> left(n,1);
     vector<int> right(n,1);
     
@@ -42,17 +51,17 @@ vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
     }
     
     for(int i = n-2; i >= 0; i--) {
-        int next = n+1;
+        int next = i+1;
         while(next < n && A[next] <= A[i]) {
             right[i] += right[next];
             next += right[next];
         }
     }
     
-    vetor<pair<int,long long>> v;
+    vector<pair<int,long long>> v;
     
     for(int i = 0; i < n; i++) {
-        v.push_back(getdivisor(A[i]), left[i] * right[i]);
+        v.push_back({getdivisor(A[i]), left[i] * right[i]});
     }
     
     sort(v.begin(), v.end(), cmd);
@@ -67,7 +76,7 @@ vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
     vector<int> ans;
     for(int i = 0; i < B.size(); i++) {
         auto id = lower_bound(currFreq.begin(), currFreq.end(), B[i]) - currFreq.begin();
-        ans.push_back(currFreq[ans].first);
+        ans.push_back(v[id].first);
     }
     
     return ans;
@@ -76,7 +85,6 @@ vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
 
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    solve({39, 99, 70, 24, 49, 13, 86, 43, 88, 74, 45, 92, 72, 71, 90, 32, 19, 76, 84, 46, 63, 15, 87, 1, 39, 58, 17, 65, 99, 43, 83, 29, 64, 67, 100, 14, 17, 100, 81, 26, 45, 40, 95, 94, 86, 2, 89, 57, 52, 91, 45 }, {1221, 360, 459, 651, 958, 584, 345, 181, 536, 116, 1310, 403, 669, 1044, 1281, 711, 222, 280, 1255, 257, 811, 409, 698, 74, 838});
     return 0;
 }
