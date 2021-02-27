31ffccf1f32a9d81d45533ab6db2e263133bb5d0
Giriraj <girirajsaigal@gmail.com>
2021-02-27 17:22:00 +0530

    Added previous code

--- DS & Algorithms/LeetCode/3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters/main.cpp
+++ DS & Algorithms/LeetCode/3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters/main.cpp
@@ -6,10 +6,55 @@
 //  Copyright © 2021 Giriraj Saigal. All rights reserved.
 //
 
+
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    int n = s.size();
+    if(n <= 1)
+        return n;
+    
+    int i = 0;
+    int j = 1;
+    map<char,int> db;
+    db[s[0]] = 0;
+    int len = 1;
+    
+    while(j < n){
+        if(db.find(s[j]) == db.end()) {
+            db[s[j]] = j;
+        }else{
+            int x = db[s[j]]+1;
+            while(i < x) {
+                db.erase(s[i]);
+                i++;
+            }
+            continue;
+        }
+        
+        if(j-i+1 > len) {
+            len = j-i+1;
+        }
+        
+        j++;
+    }
+    
+    if(j-i > len) {
+        len = j-i;
+    }
+    
+    return len;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    lengthOfLongestSubstring("abba");
     return 0;
 }
--- DS & Algorithms/LeetCode/6. ZigZag Conversion/6. ZigZag Conversion/main.cpp
+++ DS & Algorithms/LeetCode/6. ZigZag Conversion/6. ZigZag Conversion/main.cpp
@@ -6,10 +6,61 @@
 //  Copyright © 2021 Giriraj Saigal. All rights reserved.
 //
 
+
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+string convert(string s, int numRows) {
+    int n = s.size();
+    int i = 0;
+    
+    if(numRows == 1)
+        return s;
+    
+    vector<vector<char>> hex(numRows);
+    int j = 0;
+    bool down = true;
+    bool up = false;
+    
+    while(i < n) {
+        hex[j].push_back(s[i]);
+        
+        if(j == 0) {
+            down = true;
+            up = false;
+        }else if(j == numRows-1) {
+            down = false;
+            up = true;
+        }
+        
+        if(down) {
+            j++;
+        }else{
+            j--;
+        }
+        
+        i++;
+    }
+    
+    string ans = "";
+    
+    for(int i = 0; i < numRows; i++) {
+        for(char c : hex[i]) {
+            ans += c;
+        }
+    }
+    
+    return ans;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout<<convert("girirajsaigal",4);
     return 0;
 }
