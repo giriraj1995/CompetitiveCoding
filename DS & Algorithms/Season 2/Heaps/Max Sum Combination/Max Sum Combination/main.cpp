fdd677622dba468f58d4cbdef4d1e6ef30be3b87
Giriraj Saigal <girirajsaigal@gmail.com>
2021-05-18 22:25:06 +0530

    Max Sum Combination

---
+++ DS & Algorithms/Season 2/Heaps/Max Sum Combination/Max Sum Combination/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  Max Sum Combination
+//
+//  Created by Giriraj Saigal on 18/05/21.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+vector<int> solve(vector<int> &A, vector<int> &B, int C) {
+    sort(A.rbegin(), A.rend());
+    sort(B.rbegin(), B.rend());
+    
+    priority_queue<int, pair<int,int>> q;
+    vector<int> ans;
+    
+    for(int i = 0; i < A.size(); i++) {
+        q.push({A[0]+B[i], {0,i}});
+    }
+    
+    while(ans.size() < A.size()) {
+        ans.push_back(q.top().first);
+        int x = q.top().second.first;
+        int y = q.top().second.second;
+        
+        q.push({A[x+1]+B[]})
+    }
+}
+
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
