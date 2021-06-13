6d45b6584a1365972e52d247d3d596ac7edeb4e9
Giriraj Saigal <girirajsaigal@gmail.com>
2021-06-13 23:16:36 +0530

    Flatten Binary Tree to Linked List

---
+++ DS & Algorithms/Tree/Flatten Binary Tree to Linked List/Flatten Binary Tree to Linked List/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Flatten Binary Tree to Linked List
+//
+//  Created by Giriraj Saigal on 13/06/21.
+//
+
+#include <iostream>
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- DS & Algorithms/Tree/Least Common Ancestor/Least Common Ancestor/main.cpp
+++ DS & Algorithms/Tree/Least Common Ancestor/Least Common Ancestor/main.cpp
@@ -6,6 +6,60 @@
 //
 
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+//Definition for binary tree
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+bool find(TreeNode* A, int B, vector<int> &ans) {
+    if(A == NULL)
+        return false;
+    
+    ans.push_back(A->val);
+    
+    if(A->val == B)
+        return true;
+    else if(find(A->left, B, ans) || find(A->right, B, ans)) {
+        return true;
+    }
+    
+    ans.pop_back();
+    return false;
+}
+
+int lca(TreeNode* A, int B, int C) {
+    vector<int> v1;
+    vector<int> v2;
+    
+    if(!find(A,B,v1) || !find(A,C,v2)) {
+        return -1;
+    }
+    
+    int i = 0;
+    int j = 0;
+    
+    while(i < v1.size() && j < v2.size()) {
+        if(v1[i] != v2[j])
+            break;
+        
+        i++;
+        j++;
+    }
+    
+    return v1[i-1];
+}
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
