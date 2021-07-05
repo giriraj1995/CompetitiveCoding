a8b47feb9786cce612ed64b13e5f8d6ee6d760f4
Giriraj Saigal <girirajsaigal@gmail.com>
2021-07-06 00:29:49 +0530

    coins in a line

--- DS & Algorithms/Season 2/DP/Coins in a Line/Coins in a Line/main.cpp
+++ DS & Algorithms/Season 2/DP/Coins in a Line/Coins in a Line/main.cpp
@@ -16,15 +16,27 @@
 using namespace std;
 
 long long ans;
+vector<vector<vector<int>>> dp;
 
 int solve(int i, int j, vector<int>& A, bool now) {
     if(i > j){
         return 0;
     }
     
-    int a = A[i] + solve(i+1,j,A,!now);
-    int b = A[j] + solve(i,j-1,A,!now);
-    return max(a,b);
+    if(dp[i][j][now] != -1)
+        return dp[i][j][now];
+    
+    int a,b;
+    if(now) {
+        a = A[i] + solve(i+1,j,A,!now);
+        b = A[j] + solve(i,j-1,A,!now);
+        return dp[i][j][now] = max(a,b);
+    }else{
+        a = solve(i+1,j,A,!now);
+        b = solve(i,j-1,A,!now);
+        return dp[i][j][now] = min(a,b);
+    }
+    
     
 }
 
@@ -32,12 +44,15 @@ int maxcoin(vector<int> A) {
     int i = 0;
     ans = 0;
     int j = A.size()-1;
+    
+    dp = vector<vector<vector<int>>>(A.size()+1, vector<vector<int>>(A.size()+1, vector<int>(2, -1)));
     bool now = true;
-    return ans;
+    
+    return solve(i,j,A,now);
 }
 
 
 int main(int argc, const char * argv[]) {
-    maxcoin({1,2,3,4});
+    cout << maxcoin({1,2,3,4}) << endl;
     return 0;
 }
