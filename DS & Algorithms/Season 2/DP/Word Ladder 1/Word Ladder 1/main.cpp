366543a92ab32f1ffe6bd1f3cb4c8c1653e864ad
Giriraj Saigal <girirajsaigal@gmail.com>
2021-08-11 22:32:36 +0530

    word ladder 1 fix

--- DS & Algorithms/Season 2/DP/Word Ladder 1/Word Ladder 1/main.cpp
+++ DS & Algorithms/Season 2/DP/Word Ladder 1/Word Ladder 1/main.cpp
@@ -42,22 +42,55 @@ long long solve(string beginWord, string endWord, set<string> s) {
     
 int ladderLength(string beginWord, string endWord, vector<string> wordList) {
     set<string> s;
+    
     for(auto k : wordList){
         s.insert(k);
     }
-    
+
     if(s.find(endWord) == s.end())
         return 0;
     
-    long long ans = solve(beginWord, endWord, s);
+    queue<pair<string,int>> q;
+    q.push({beginWord,1});
     
-    if(ans == (long long)INT_MAX + (long long)1)
-        return 0;
+    map<string, int> mp;
     
-    return ans;
+    for(int i = 0; i < wordList.size(); i++) {
+        mp[wordList[i]] = 1000000;
+    }
+    
+    while(q.size() > 0) {
+        string x = q.front().first;
+        int level = q.front().second;
+        q.pop();
+        
+        if(x == endWord)
+            return mp[x];
+        
+        string f = x;
+        for(char c = 'a'; c <= 'z'; c++) {
+            for(int i = 0; i < x.size(); i++) {
+                char t = x[i];
+                x[i] = c;
+                
+                if(s.find(x) != s.end() && x!=f) {
+                    if(level+1 < mp[x]) {
+                        mp[x] = level+1;
+                        q.push({x,level+1});
+                    }
+                }
+                x[i] = t;
+            }
+        }
+    }
+    
+    return 0;
+    
 }
 
 int main(int argc, const char * argv[]) {
-    cout << ladderLength("hot", "dog", {"hot","dog"}) <<endl;
+    cout << ladderLength("hit",
+                         "cog",
+                         {"hot","dot","tog","cog"}) <<endl;
     return 0;
 }
