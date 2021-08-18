aedb21844e6a345c2e88bee20e00c07d5be9dbc8
Giriraj Saigal <girirajsaigal@gmail.com>
2021-08-18 23:59:23 +0530

    Egg drop problem

---
+++ DS & Algorithms/Season 2/DP/Egg Drop problem/Egg Drop problem/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  Egg Drop problem
+//
+//  Created by Giriraj Saigal on 18/08/21.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+vector<vector<int>> dp;
+int eggdrop(int eggs, int floors) {
+    if(eggs == 1)
+        return dp[eggs][floors] = floors;
+    
+    if(floors == 1)
+        return dp[eggs][floors] = 1;
+    
+    if(dp[eggs][floors] != -1){
+        return dp[eggs][floors];
+    }
+        
+    int tempans = floors;
+    for(int i = 1; i <= floors; i++) {
+        //breaks
+        int whenbreaks = dp[eggs-1][i-1] = eggdrop(eggs-1, i-1);
+        //does not break
+        int whendoesntbreak = dp[eggs][floors-i] = eggdrop(eggs, floors-i);
+        
+        tempans = min(tempans, 1 + max(whenbreaks,whendoesntbreak));
+    }
+    
+    return dp[eggs][floors] = tempans;
+}
+
+int solve(int A, int B) {
+    dp = vector<vector<int>>(A+1, vector<int>(B+1, -1));
+    return eggdrop(A,B);
+}
+
+int main(int argc, const char * argv[]) {
+    cout << solve(2, 10);
+    return 0;
+}
