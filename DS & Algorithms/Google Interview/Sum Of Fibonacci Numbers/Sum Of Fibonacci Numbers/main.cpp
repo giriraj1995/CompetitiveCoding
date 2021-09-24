5f84f7517efd094aad9779f0f3074e0c3d1dbc9f
Giriraj Saigal <girirajsaigal@gmail.com>
2021-09-24 23:21:17 +0530

    Sum of fibo

---
+++ DS & Algorithms/Google Interview/Sum Of Fibonacci Numbers/Sum Of Fibonacci Numbers/main.cpp
@@ -0,0 +1,77 @@
+//
+//  main.cpp
+//  Sum Of Fibonacci Numbers
+//
+//  Created by Giriraj Saigal on 24/09/21.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+vector<int> dp;
+
+int findInd(int x, int i, int j) {
+    int ans = -1;
+    
+    while(i <= j) {
+        int m = i + (j-i)/2;
+        if(dp[m] == x)
+            return m;
+        else{
+            if(x < dp[m]){
+                ans = m;
+                j = m-1;
+            }else{
+                i = m+1;
+            }
+        }
+    }
+    
+    return ans;
+}
+
+int solve(int A, int k) {
+    
+    if(A == 0 || A == 1)
+        return A;
+    
+    int i;
+    for(i = k; i>0; i--) {
+        if(dp[i] <= A) {
+            A -= dp[i];
+            break;
+        }
+    }
+    
+    return 1+solve(A, findInd(A, 0, i));
+}
+
+int fibsum(int A) {
+    
+    if(A == 0 || A == 1)
+        return A;
+    
+    dp = {};
+    dp.push_back(1);
+    dp.push_back(1);
+    int i = 2;
+    
+    while(dp[i-1]+dp[i-2] <= A) {
+        dp.push_back(dp[i-1]+dp[i-2]);
+        i++;
+    }
+    
+    return solve(A, dp.size()-1);
+}
+
+
+int main(int argc, const char * argv[]) {
+    cout << fibsum(100) << endl;
+    return 0;
+}
