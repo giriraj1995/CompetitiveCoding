4fbc393dfce3f0cb6d8313183c2bb0d6b44d588e
Giriraj Saigal <girirajsaigal@gmail.com>
2021-10-18 23:40:32 +0530

    502. IPO

--- DS & Algorithms/Google Interview/502. IPO/502. IPO/main.cpp
+++ DS & Algorithms/Google Interview/502. IPO/502. IPO/main.cpp
@@ -14,35 +14,53 @@
 #include <queue>
 #include <set>
 using namespace std;
+#define pii pair<int,int>
 
-int solve(int n, int k, int w, vector<int>& profits, vector<int>& capital) {
-        if(n == profits.size())
-            return 0;
-        if(k == 0)
-            return 0;
-        if(w < 0)
-            return 0;
-        
-        
-        if(capital[n] <= w) {
-           //do work project
-            int x = profits[n] + solve(n+1, k-1, w+capital[n], profits, capital);
-            //donot work on project
-            int y = solve(n+1, k, w, profits, capital);
-            
-            return max(x,y);
-        }else{
-            //donot work on project
-            int x = solve(n+1, k, w, profits, capital);
-            return x;
+int findMaximizedCapital(int k, int w, vector<int> profits, vector<int> capital) {
+        priority_queue<pii> pro;
+        priority_queue<pii, vector<pii>, greater<pii>> cap;
+        int n = profits.size();
+        for(int i = 0; i < n; i++) {
+            cap.push({capital[i], profits[i]});
         }
+        
+        int curr = w;
+        int ans = 0;
+        
+        while(cap.size() > 0) {
+            int currcap = cap.top().first;
+            int currpro = cap.top().second;
+
+            if(currcap <= curr){
+                cap.pop();
+                pro.push({currpro, currcap});
+            }else{
+                if(pro.size() == 0 || k == 0)
+                    break;
+                else{
+                    int prepro = pro.top().first;
+                    pro.pop();
+                    
+                    ans += prepro;
+                    k--;
+                    curr += prepro;
+                }
+            }
+        }
+    
+    while(k > 0 && pro.size() > 0) {
+        int prepro = pro.top().first;
+        pro.pop();
+        
+        ans += prepro;
+        k--;
+        curr += prepro;
     }
     
-    int findMaximizedCapital(int k, int w, vector<int> profits, vector<int> capital) {
-        return solve(0,k,w,profits,capital);
-    }
+    return curr;
+}
 
 int main(int argc, const char * argv[]) {
-    findMaximizedCapital(2,0,{1,2,3},{0,1,2});
+    findMaximizedCapital(1,2,{1,2,3},{1,1,2});
     return 0;
 }
