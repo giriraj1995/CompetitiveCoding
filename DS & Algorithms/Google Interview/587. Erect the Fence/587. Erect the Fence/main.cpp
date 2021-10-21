60b89eec5dd28a0e02bea9b335dbfbcd0c1be66a
Giriraj Saigal <girirajsaigal@gmail.com>
2021-10-21 22:57:39 +0530

    Erect the fence

--- DS & Algorithms/Google Interview/587. Erect the Fence/587. Erect the Fence/main.cpp
+++ DS & Algorithms/Google Interview/587. Erect the Fence/587. Erect the Fence/main.cpp
@@ -15,55 +15,117 @@
 #include <set>
 using namespace std;
 
-int cartiansProduct(vector<int> a, vector<int> b, vector<int> c) {
-        return (c[1] - a[1]) * (b[0] - a[0]) - (b[1] - a[1]) * (c[0] - a[0]);
+//int cartiansProduct(vector<int> a, vector<int> b, vector<int> c) {
+//        return (c[1] - a[1]) * (b[0] - a[0]) - (b[1] - a[1]) * (c[0] - a[0]);
+//    }
+    
+//vector<vector<int>> outerTrees(vector<vector<int>> trees) {
+//        int n = trees.size();
+//
+//        if(n <= 3)
+//            return trees;
+//
+//        set<vector<int>> uniqueSet;
+//        map<vector<int>, int> mp;
+//        int leftModeX = 0;
+//
+//
+//        //find the most left most index of trees
+//        for(int i = 0; i < trees.size(); i++) {
+//            mp.insert({trees[i],i});
+//            if(trees[i][0] < trees[leftModeX][0]) {
+//                leftModeX = i;
+//            }
+//        }
+//        //{{1,1},{2,2},{2,0},{2,4},{3,3},{4,2}}
+//
+//        int curr = leftModeX;
+//        int cand = (leftModeX+1) % n;
+//        uniqueSet.insert(trees[leftModeX]);
+//
+//        while(cand != leftModeX) {
+//            cand = (curr+1) % n;
+//
+//            for(auto k : mp) {
+//                if(cartiansProduct(trees[curr], trees[k.second], trees[cand]) > 0) {
+//                    cand = k.second;
+//                }
+//            }
+//
+//            for(auto k : mp) {
+//                if(cartiansProduct(trees[curr], trees[cand], trees[k.second]) == 0) {
+//                    uniqueSet.insert(trees[k.second]);
+//                }
+//            }
+//
+//            mp.erase(trees[cand]);
+//            uniqueSet.insert(trees[cand]);
+//            curr = cand;
+//        }
+//
+//        vector<vector<int>> ans;
+//
+//        for(auto k : uniqueSet)
+//            ans.push_back(k);
+//
+//        return ans;
+//    }
+
+int slope(vector<int> l2, vector<int> l1, vector<int> l) {
+        int x2 = l2[0];
+        int y2 = l2[1];
+        
+        int x1 = l1[0];
+        int y1 = l1[1];
+        
+        int x = l[0];
+        int y = l[1];
+        
+        int t =  (y - y1)*(x1 - x2) - (y1 - y2)*(x - x1) ;
+    return t;
     }
     
-    vector<vector<int>> outerTrees(vector<vector<int>> trees) {
+vector<vector<int>> outerTrees(vector<vector<int>> trees) {
         int n = trees.size();
-        
         if(n <= 3)
             return trees;
         
-        set<vector<int>> uniqueSet;
-        int leftModeX = 0;
+        sort(trees.begin(), trees.end());
         
+        vector<vector<int>> lower;
+        vector<vector<int>> higher;
         
-        //find the most left most index of trees
         for(int i = 0; i < trees.size(); i++) {
-            if(trees[i][0] < trees[leftModeX][0]) {
-                leftModeX = i;
-            }
-        }
-        //{{1,1},{2,2},{2,0},{2,4},{3,3},{4,2}}
-        
-        int curr = leftModeX;
-        int cand = (leftModeX+1) % n;
-        
-        while(cand != leftModeX) {
-            uniqueSet.insert(trees[curr]);
-            cand = (curr+1) % n;
-            for(int i = 0; i < n; i++) {
-                if(cartiansProduct(trees[curr], trees[i], trees[cand]) > 0) {
-                    cand = i;
-                }
-            }
             
-            for(int i = 0; i < n; i++) {
-                if(cartiansProduct(trees[curr], trees[cand], trees[i]) == 0) {
-                    uniqueSet.insert(trees[i]);
-                }
+        while(lower.size() >= 2 && slope(lower[lower.size()-2], lower[lower.size()-1], trees[i]) < 0){
+                lower.pop_back();
             }
-            
-            curr = cand;
+                
+        while(higher.size() >= 2 && slope(higher[higher.size()-2], higher[higher.size()-1], trees[i]) > 0){
+                higher.pop_back();
+            }
+                
+            lower.push_back(trees[i]);
+            higher.push_back(trees[i]);
         }
         
-        vector<vector<int>> ans;
+        set<vector<int>> ans;
         
-        for(auto k : uniqueSet)
-            ans.push_back(k);
+        for(auto k : lower) {
+            ans.insert(k);
+        }
         
-        return ans;
+        for(auto k : higher) {
+            ans.insert(k);
+        }
+        
+        vector<vector<int>> vec;
+        
+        for(auto k : ans){
+            vec.push_back(k);
+        }
+        
+        return vec;
     }
 
 int main(int argc, const char * argv[]) {
