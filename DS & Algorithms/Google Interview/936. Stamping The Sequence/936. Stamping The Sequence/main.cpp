a7b5fd16f3307dba0b73fd81918eb581c7221e0e
Giriraj Saigal <girirajsaigal@gmail.com>
2021-11-18 23:59:52 +0530

    ok

---
+++ DS & Algorithms/Google Interview/936. Stamping The Sequence/936. Stamping The Sequence/main.cpp
@@ -0,0 +1,80 @@
+//
+//  main.cpp
+//  936. Stamping The Sequence
+//
+//  Created by Giriraj Saigal on 18/11/21.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <set>
+using namespace std;
+
+vector<int> movesToStamp(string stamp, string target) {
+        char firstLetter = stamp[0];
+        vector<int> inds;
+        
+        for(int i = 0; i < target.size(); i++)
+            if(firstLetter == target[i])
+                inds.push_back(i);
+        
+        if(inds.size() == 0)
+            return {};
+        
+        if(inds.size() == target.size()){
+            return inds;
+        }
+        
+        queue<pair<string, vector<int>>> q;
+        q.push({string(target.size(), '?'), {}});
+        int turn = 0;
+        
+        while(q.size() > 0) {
+            
+            int x = q.size();
+            
+            if(turn > 10 * target.size())
+                return {};
+            
+            for(int i = 0; i < x; i++) {
+                string curr = q.front().first;
+                vector<int> vec = q.front().second;
+                q.pop();
+                
+                if(curr == target)
+                    return vec;
+                    
+                for(int i = 0; i < inds.size(); i++) {
+                    if(vec.size() > 0 && inds[i] == vec[vec.size()-1])
+                        continue;
+                    
+                    vector<int> f = vec;
+                    f.push_back(inds[i]);
+                    int prev = inds[i];
+                    int sn = stamp.size();
+                    int next = target.size() - sn - prev;
+                    string t;
+                    if(next < 0) {
+                        t = curr.substr(0,inds[i]) +  stamp.substr(0, target.size() - inds[i]);
+                    }else{
+                        t = curr.substr(0,inds[i]) + stamp + curr.substr(inds[i] + stamp.size());
+                    }
+                    q.push({t, f});
+                }
+            }
+            
+            turn++;
+        }
+        
+        return {};
+    }
+
+int main(int argc, const char * argv[]) {
+    movesToStamp("abca","aabcaca");
+    return 0;
+}
